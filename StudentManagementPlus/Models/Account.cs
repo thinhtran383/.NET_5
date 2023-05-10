cb5d2963fb3ab07800e94b618656115c7bc31d61
Thịnh Trần <thinhtran3803@gmail.com>
2023-05-10 20:42:55 +0700

    add model folder and Account class

---
+++ StudentManagementPlus/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace StudentManagementPlus.Models {
+    public class Account {
+        private string Username { get; set; }
+        private string Password { get; set; }
+
+        public Account(string Username, string Password) {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+    }
+}
