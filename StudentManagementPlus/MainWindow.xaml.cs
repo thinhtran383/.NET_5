388376023e7d2c5a046e85f4835f870bf7d6c28c
Thịnh Trần <thinhtran3803@gmail.com>
2023-05-10 20:59:20 +0700

    done login

--- StudentManagementPlus/MainWindow.xaml.cs
+++ StudentManagementPlus/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,60 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using StudentManagementPlus.Models;
 
 namespace StudentManagementPlus {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow:Window {
+        private static string connectionString = @"Data Source=THINHTRAN\MSSQLSERVER02;Initial Catalog=quanlysinhvien;Integrated Security=True;";
+        
+        private static List<Account> accounts = new List<Account>();
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void btnSignin_Click(object sender,RoutedEventArgs e) {
+
+            try {
+                string sql = "Select * from adminaccount";
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql,connection);
+                connection.Open();
+                var data = command.ExecuteReader();
+                while (data.Read()) {
+                    string username = data.GetString(data.GetOrdinal("username"));
+                    string password = data.GetString(data.GetOrdinal("password"));
+                    Account account = new Account(username,password);
+                    accounts.Add(account);
+                }
+            } catch(SqlException E) {
+                MessageBox.Show("Lỗi khi truy vấn cớ sở dữ liệu ");
+                throw;
+            }
+
+            string usernameInput = tbUsername.Text;
+            string passwordInput = tbPassword.Password;
+
+            if(tbPassword.Password == "" || tbUsername.Text == "") {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+                return;
+            }
+
+            foreach(var account in accounts) {
+                if(account.Username == usernameInput && account.Password == passwordInput) {
+                    MessageBox.Show("Đăng nhập thành công");
+                    Panel panel = new Panel();
+                    this.Close();
+                    panel.Show();
+                    return;
+                }
+                else {
+                    MessageBox.Show("Thông tin tài khoản hoặc mật khẩu không chính xác");
+                }
+            }
         }
     }
 }
