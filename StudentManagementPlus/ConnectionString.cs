1df916093670168c2fd2010b4a9555953b9e064f
Thịnh Trần <thinhtran3803@gmail.com>
2023-05-10 23:51:30 +0700

    string connect

---
+++ StudentManagementPlus/ConnectionString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementPlus {
+    public class ConnectionString {
+        public static string connectionString = @"Data Source=THINHTRAN\MSSQLSERVER02;Initial Catalog=quanlysinhvien;Integrated Security=True;";
+    }
+}
